961ffaac93e8d27b019e8f19acb3ddc49c704f58
Ben Bagley <hello@benbagley.dev>
2019-10-17 23:10:39 +0100

    Extra bracket removed

--- models.js
+++ models.js
@@ -30,7 +30,7 @@ userSchema.statics.hashPassword = function(password) {
 };
 
 userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.Password); };
+  return bcrypt.compareSync(password, this.Password);
 };
 
 const Movie = mongoose.model("Movie", movieSchema);
